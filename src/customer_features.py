c2827547c3fde4f4484ef7d662a5e8603521d001
David Schaaf <davidschaaf@berkeley.edu>
2026-02-19 15:36:19 -0500

    Calculating recency frequency monetary metric for customer

--- src/customer_features.py
+++ src/customer_features.py
@@ -1,9 +1,27 @@
 import pandas as pd
 import numpy as np
-from pathlib import Path
 
-class CustomerFeatureEngineering:
-    def __init__(self, customers_path, transactions_path):
-        self.customers = pd.read_csv(customers_path)
-        self.transactions = pd.read_csv(transactions_path)
+class CustomerFeatureEngineer:
+    def __init__(self, customers_df, transactions_df):
+        self.customers = customers_df
+        self.transactions = transactions_df
+        self.set_dtypes()
+    
+    def calculate_rfm(self, as_of_date = None):
+        if as_of_date == None:
+            as_of_date == self.transactions['t_dat'].max()
+        as_of_date = pd.to_datetime(as_of_date)
 
+        relevant_txn = self.transactions[self.transactions['t_dat'] <= as_of_date]
+        rfm = relevant_txn.groupby('customer_id').agg(
+            last_purchase_date = ('t_dat', 'max'),
+            num_purchases = ('t_dat', 'count'),
+            total_spent = ('price','sum')
+        ).reset_index()
+
+        rfm['days_since_last_purchase'] = (as_of_date - rfm['last_purchase_date']).dt.days
+        rfm = rfm.drop('last_purchase_date', axis=1)
+        return rfm
+
+    def set_dtypes(self):
+        self.transactions['t_dat'] = pd.to_datetime(self.transactions['t_dat'])
--- test/test_customer_features.py
+++ test/test_customer_features.py
@@ -1,13 +1,50 @@
 import pytest
 import pandas as pd
-from src.customer_feature_engineering import CustomerFeatureEngineering
+from src.customer_features import CustomerFeatureEngineer
 from pathlib import Path
 
 data_path = Path("./data")
 customers_path = data_path / 'customer_hm_cleaned.csv'
 transactions_path = data_path / 'transactions_hm_cleaned.csv'
+customers_df = pd.read_csv(customers_path)
+transactions_df = pd.read_csv(transactions_path)
 
-def test_customer_feature_engineering():
-    cfe = CustomerFeatureEngineering(customers_path, transactions_path)
+@pytest.fixture
+def cfe():
+    return CustomerFeatureEngineer(customers_df, transactions_df)
+
+def test_customer_feature_engineer_init(cfe):
     assert isinstance(cfe.customers, pd.DataFrame)
     assert isinstance(cfe.transactions, pd.DataFrame)
+    assert pd.api.types.is_datetime64_any_dtype(cfe.transactions['t_dat'])
+
+def test_rfm_returns_correct_columns(cfe):
+    result = cfe.calculate_rfm(as_of_date='2019-10-31')
+    assert 'customer_id' in result.columns
+    assert 'days_since_last_purchase' in result.columns
+    assert 'num_purchases' in result.columns
+    assert 'total_spent' in result.columns
+
+def test_rfm_correct_types(cfe):
+    result = cfe.calculate_rfm(as_of_date='2019-10-31')
+    assert result['days_since_last_purchase'].dtype == 'int64'
+    assert result['num_purchases'].dtype == 'int64'
+    assert result['total_spent'].dtype == 'float64'
+
+def test_rfm_with_single_customer():
+    test_transactions = pd.DataFrame({
+        't_dat': ['2019-10-01', '2019-10-15'],
+        'customer_id': ['cust_1', 'cust_1'],
+        'article_id': [123, 456],
+        'price': [0.01, 0.02]
+    })
+    test_customers = pd.DataFrame({
+        "customer_id": ['cust_1']
+    })
+    
+    fe = CustomerFeatureEngineer(test_customers, test_transactions)
+    result = fe.calculate_rfm(as_of_date='2019-10-31')
+    
+    assert result.loc[result['customer_id'] == 'cust_1', 'days_since_last_purchase'].values[0] == 16 
+    assert result.loc[result['customer_id'] == 'cust_1', 'num_purchases'].values[0] == 2
+    assert result.loc[result['customer_id'] == 'cust_1', 'total_spent'].values[0] == 0.03

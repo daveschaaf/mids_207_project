4f626aa60729c63cc6878ee32e08788baa65801e
David Schaaf <davidschaaf@berkeley.edu>
2026-02-20 11:52:08 -0500

    positive and negative examples are created

--- src/recommendation_training.py
+++ src/recommendation_training.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 class RecommendationTrainingBuilder:
     def __init__(self, transactions_df, customer_features_df, product_features_df):
         self.transactions = transactions_df
@@ -20,4 +19,46 @@ class RecommendationTrainingBuilder:
         
         positives = relevant_txn[['customer_id', 'article_id']].drop_duplicates()
         positives['purchased'] = 1
+
+        positives = positives.merge(
+            self.product_features,
+            on='article_id',
+            how='left'
+        )
+
+        positives = positives.merge(
+            self.customer_features,
+            on='customer_id',
+            how='left'
+        )
         return positives
+    
+    def _sample_negative_examples(self, positives, negative_ratio=2, random_state=67):
+        customer_ids = self.customer_features['customer_id'].unique()
+        article_ids = self.product_features['article_id'].unique()
+
+        negative_examples = []
+        for customer in customer_ids:
+            bought = positives[positives['customer_id'] == customer].values
+
+            non_purchase_products = [art_id for art_id in article_ids if art_id not in bought]
+            sampled_products = pd.Series(non_purchase_products).sample(n=negative_ratio, random_state=random_state)
+
+            for product in sampled_products:
+                negative_examples.append({
+                    'customer_id': customer,
+                    'article_id': product,
+                    'purchased': 0
+                })
+        negatives = pd.DataFrame(negative_examples)
+        negatives = negatives.merge(
+            self.product_features,
+            on='article_id',
+            how='left'
+        )
+        negatives = negatives.merge(
+            self.customer_features,
+            on='customer_id',
+            how='left'
+        )
+        return negatives
--- test/test_recommendation_training.py
+++ test/test_recommendation_training.py
@@ -30,3 +30,78 @@ def test_get_positive_examples():
     assert set(positives['article_id'].values) == {456, 789}
     assert 'purchased' in positives.columns
     assert (positives['purchased'] == 1).all()
+
+
+def test_get_positive_examples_with_features():
+    """Test that positive examples include customer and product features"""
+    transactions = pd.DataFrame({
+        't_dat': pd.to_datetime(['2019-10-05', '2019-10-15']),
+        'customer_id': ['c1', 'c2'],
+        'article_id': [456, 789],
+        'price': [0.02, 0.03]
+    })
+    
+    customer_features = pd.DataFrame({
+        'customer_id': ['c1', 'c2'],
+        'days_since_last_purchase': [5, 10],
+        'num_purchases': [3, 7]
+    })
+    
+    product_features = pd.DataFrame({
+        'article_id': [456, 789],
+        'avg_price': [0.02, 0.03],
+        'sales_last_7_days': [10, 20]
+    })
+    
+    builder = RecommendationTrainingBuilder(transactions, customer_features, product_features)
+    positives = builder._get_positive_examples(
+        prediction_start='2019-10-01',
+        prediction_end='2019-10-31'
+    )
+    
+    assert len(positives) == 2
+    assert 'purchased' in positives.columns
+    assert 'days_since_last_purchase' in positives.columns
+    assert 'num_purchases' in positives.columns
+    assert 'avg_price' in positives.columns
+    assert 'sales_last_7_days' in positives.columns
+    c1_row = positives[positives['customer_id'] == 'c1'].iloc[0]
+    assert c1_row['days_since_last_purchase'] == 5
+    assert c1_row['avg_price'] == 0.02
+    assert c1_row['purchased'] == 1
+
+def test_sample_negative_examples():
+    """Test negative sampling - customers paired with products they didn't buy"""
+    positives = pd.DataFrame({
+        'customer_id': ['c1', 'c2'],
+        'article_id': [456, 789],
+        'purchased': [1, 1]
+    })
+    
+    transactions = pd.DataFrame({
+        't_dat': pd.to_datetime(['2019-10-05', '2019-10-15']),
+        'customer_id': ['c1', 'c2'],
+        'article_id': [456, 789],
+        'price': [0.02, 0.03]
+    })
+    
+    customer_features = pd.DataFrame({
+        'customer_id': ['c1', 'c2'],
+        'days_since_last_purchase': [5, 10]
+    })
+    
+    product_features = pd.DataFrame({
+        'article_id': [123, 456, 789, 999],
+        'avg_price': [0.01, 0.02, 0.03, 0.04]
+    })
+    
+    builder = RecommendationTrainingBuilder(transactions, customer_features, product_features)
+    
+    negatives = builder._sample_negative_examples(positives, negative_ratio=2, random_state=42)
+    assert len(negatives) == 4
+    assert (negatives['purchased'] == 0).all()
+    negative_pairs = set(zip(negatives['customer_id'], negatives['article_id']))
+    positive_pairs = set(zip(positives['customer_id'], positives['article_id']))
+    assert len(negative_pairs & positive_pairs) == 0
+    assert 'days_since_last_purchase' in negatives.columns
+    assert 'avg_price' in negatives.columns
